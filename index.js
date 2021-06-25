3855deff2a45722d9ac1df1c843c6243005339ab
amongiove <amandamongiove@gmail.com>
2021-06-25 15:31:26 -0400

    commit tree d90f9fa43f7b4722612896c3ab0c9228686c23c8

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+
+function mapToNegativize(array) {
+   return array.map(negativize)  
+}
+
+    function negativize(num) {
+        return (-1 * num);
+    }
+
+function mapToNoChange(array) {
+    return array.map(noChange)
+}
+
+    function noChange(num) {
+        return num
+    }
+
+function mapToDouble(array) {
+    return array.map(double)
+}
+
+    function double(num) {
+        return (2 * num)
+    }
+
+function mapToSquare(array) {
+    return array.map(square)
+}
+
+    function square(num) {
+        return (num * num)
+    }
+
+function reduceToTotal(array, start=0) {
+    return array.reduce(reducer, start)
+}
+
+    function reducer(accumulator, start) {
+        return accumulator + start
+    }
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+        if (!src[i]) return false
+    }
+    return true
+}
+    
+function reduceToAnyTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+        if (src[i]) return true
+    }
+    return false
+}
